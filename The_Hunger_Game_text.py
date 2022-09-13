6ea831f1ef487a9c69d61e0213a93cccf37e7c23
AnnaWillis <arw2451@gmail.com>
2022-09-12 18:16:53 -0700

    Task class and researched operators

--- The_Hunger_Game_text.py
+++ The_Hunger_Game_text.py
@@ -20,8 +20,9 @@ class State:
         self.wd = 5 # waste during distribution/transit - same units
         self.bh = 80 # percent of food distributed that people buy from corporations
         self.ch = 70 # percent of food that people buy, that they eat
-        self.m = 500 # amount of money available - change later
+        self.m = 1000 # amount of money available - change later
         self.t = 0 # time passed
+        self.rocket_truck = False
         if old is not None:
             self.p = old.p
             self.wp = old.wp
@@ -30,6 +31,7 @@ class State:
             self.ch = old.ch
             self.m = old.m
             self.t = old.t
+            self.rocket_truck = old.rocket_truck
         self.d = 0
         self.calc_total_distribution()
         self.w = 0
@@ -55,18 +57,21 @@ class State:
         self.h = 100 - (self.p - self.w)
         return max(0, self.h)
 
-    def move(self, dp, dwp, dwd, dbh, dch, cost):
+    def move(self, t):
         # global ROOT
         new = State(self)
-        new.p += dp
-        new.wp += dwp
-        new.wd += dwd
-        new.bh += dbh
-        new.ch += dch
-        new.m -= cost
+        new.p += t.dp
+        new.wp += t.dwp
+        new.wd += t.dwd
+        new.bh += t.dbh
+        new.ch += t.dch
+        new.m -= t.cost
         new.d = new.calc_total_distribution()
         new.w = new.calc_total_waste()
         new.h = new.calc_hunger()
+        t.done()
+        if not t.can_do_again(): t.set_name("Unavailable") # truth be told, idk if this works, but I figured I'd try it so people don't keep trying to use it from the visual dropdown menu
+        if t.times_used == 1: print(t.get_message())
         # if self.is_goal():
         #     try:
         #         holdwindow.destroy()
@@ -75,11 +80,14 @@ class State:
         #         print(type(holdwindow))
         return new
 
-    def can_move(self, dp, dwp, dwd, dbh, dch, cost):
-        if self.wp + self.wd + dwp + dwd > self.p + dp: return False
-        if self.bh + dbh > 100 or self.bh + dbh < 0: return False
-        if self.ch + dch > 100 or self.ch + dch < 0: return False
-        if cost > self.m: return False
+    def can_move(self, t):
+        global task5, task6
+        if task5.times_used == 0 and t.name == task6.name: return False
+        if not t.can_do_again(): return False
+        if self.wp + self.wd + t.dwp + t.dwd > self.p + t.dp: return False
+        if self.bh + t.dbh > 100 or self.bh + t.dbh < 0: return False
+        if self.ch + t.dch > 100 or self.ch + t.dch < 0: return False
+        if t.cost > self.m: return False
         return True
 
     # \u001b[38;5;##m
@@ -121,6 +129,35 @@ class State:
 def copy_state(s):
     return State(old=s)
 
+class Task:
+
+    def __init__(self, name, msg, dp, dwp, dwd, dbh, dch, cost, max_times):
+        self.name = name
+        self.msg = msg
+        self.dp = dp
+        self.dwp = dwp
+        self.dwd = dwd
+        self.dbh = dbh
+        self.dch = dch
+        self.cost = cost
+        self.max_times = max_times
+        self.times_used = 0
+
+    def set_name(self, name):
+        self.name = name
+
+    def can_do_again(self):
+        return self.times_used < self.max_times
+    
+    def done(self):
+        self.times_used += 1
+
+    def get_message(self):
+        return str(self.msg)
+
+    def __str__(self):
+        return str(self.name)
+
 class Operator:
 
   def __init__(self, name, precond, state_transf):
@@ -138,42 +175,45 @@ INITIAL_STATE = State()
 
 GOAL_MESSAGE_FUNCTION = lambda s: s.goal_message()
 
-# (self, dp, dwp, dwd, dbh, dch, cost)
+# cost is in millions
+# (self, name, msg, dp, dwp, dwd, dbh, dch, cost, max_times)
 
-phi0 = Operator("Campaign against household food waste", lambda s: s.can_move(0, 0, 0, -1, 5, 50),\
-    lambda s: s.move(0, 0, 0, -1, 5, 50))
+name = "Give silos to farms in need."
+msg = "Some small-scale farms do not have access to modern storage equipment, or it is simply too expensive in the short-term. Changes so simple as supplying them with silos can cut their post-harvest losses from 40% to 2%! Thanks to your contribution, less food in Dennyville will go to waste."
+task0 = Task(name, msg, 0, -3, 0, 0, 0, 165, 3)
+phi0 = Operator(task0.name, lambda s: s.can_move(task0), lambda s: s.move(task0))
 
-phi1 = Operator("Constant bonfire of usable food", lambda s: s.can_move(0, 0, 0, 0, -20, 100),\
-    lambda s: s.move(0, 0, 0, 0, -20, 100))
+name = "Reconstruct federal and state roads nearby and leading into Dennyville."
+msg = "Trucking is vital to farms’ success. In the United States, 70% of agricultural and food products travel by truck to their destinations. However, many roads aren’t in the best shape, which hinders transportation, especially of perishable goods. Because you fixed up major roadways in the Dennyville area, less food will be wasted in transit!"
+task1 = Task(name, msg, 0, 0, -7, 0, 0, 1500, 1)
+phi1 = Operator(task1.name, lambda s: s.can_move(task1), lambda s: s.move(task1))
 
-phi2 = Operator("Develop better pesticides", lambda s: s.can_move(10, 0, 0, 0, 0, 20),\
-    lambda s: s.move(10, 0, 0, 0, 0, 20))
+name = "Launch a month-long ad campaign to bring awareness to household food waste."
+msg = "You’ve pestered all of Dennyville with your relentless ads… but maybe that’s a good thing. A lot of people don’t know that households generate 31% of all food waste in industrialized countries. By encouraging Dennyville residents to build habits such as planning meals, eating leftovers, using the freezer to prolong shelf life, and donating excess food, the area has seen a decrease in food waste."
+task2 = Task(name, msg, 0, 0, 0, -5, 15, 50, 1)
+phi2 = Operator(task2.name, lambda s: s.can_move(task2), lambda s: s.move(task2))
 
-phi3 = Operator("Improve road infrastructre", lambda s: s.can_move(0, -5, 0, 0, 0, 40),\
-    lambda s: s.move(0, -5, 0, 0, 0, 40))
+name = "Provide Dennyville residents with stimulus checks."
+msg = "Its effect on better-off individuals is limited, but the stimulus check prompted a significant increase in spending among lower-income residents, who are now able to put more money toward food. Dennyville thanks you for the boost, although some residents still have an issue with “free money.”"
+task3 = Task(name, msg, 0, 0, 0, 7, 0, 1000, 1)
+phi3 = Operator(task3.name, lambda s: s.can_move(task3), lambda s: s.move(task3))
 
-phi4 = Operator("Innovate new farming technology", lambda s: s.can_move(25, -10, 0, 0, 0, 25),\
-    lambda s: s.move(25, -10, 0, 0, 0, 25))
+name = "Provide Dennyville students in low-income families with free school lunch."
+msg = "School lunch has a surprisingly large impact on hunger, with a 14% reduction in food insufficiency in United States households with one or more children receiving free or reduced-price school lunch. Dennyville’s families are very happy with your choice."
+task4 = Task(name, msg, 0, 0, 0, 7, 0, 100, 1)
+phi4 = Operator(task4.name, lambda s: s.can_move(task4), lambda s: s.move(task4))
 
-phi5 = Operator("Give silos to farms in need", lambda s: s.can_move(0, -15, 0, 0, 0, 10),\
-    lambda s: s.move(0, -15, 0, 0, 0, 10))
+name = "Appear in Elon Musk’s mind and convince him to invent the rocket truck!"
+msg = "Rocket trucks are so very fast, but they also liquidate their cargo and sometimes other vehicles. Ironically, with this advance in technology, you’ve caused the trucking industry a great setback and created a whole bunch of food waste. You’re lucky everyone is mad at Elon Musk and not the god that sent him down this wretched path."
+task5 = Task(name, msg, 0, 0, 50, 0, 0, 500, 1)
+phi5 = Operator(task5.name, lambda s: s.can_move(task5), lambda s: s.move(task5))
 
-phi6 = Operator("Farmers get tax cuts!", lambda s: s.can_move(15, -5, 0, 5, 0, 30),\
-    lambda s: s.move(15, -5, 0, 5, 0, 30))
-
-phi7 = Operator("Improve truck insulation", lambda s: s.can_move(0, -15, 0, 0, 0, 15),\
-    lambda s: s.move(0, -15, 0, 0, 0, 15))
-
-phi8 = Operator("Invent the rocket truck!", lambda s: s.can_move(0, 0, 50, 0, 0, 100),\
-    lambda s: s.move(0, 0, 50, 0, 0, 100))
-
-phi9 = Operator("Give people stimulus checks", lambda s: s.can_move(0, 0, 0, 20, 20, 75),\
-    lambda s: s.move(0, 0, 0, 20, 20, 75))
-
-phi10 = Operator("Raise sales taxes", lambda s: s.can_move(0, 20, 0, -15, -15, -50),\
-    lambda s: s.move(0, 20, 0, -15, -15, -50))
+name = "Ban the rocket truck..."
+msg = "As cool as rocket trucks are, you made the right choice. Maybe now Dennyville can begin to heal. (Though some people still use rocket trucks because they don’t care about the law and they’re too fast to get pulled over.)"
+task6 = Task(name, msg, 0, 0, -45, 0, 0, 250, 1)
+phi6 = Operator(task6.name, lambda s: s.can_move(task6), lambda s: s.move(task6))
 
 # TODO: add money operator
 # TODO: add other negative operators
 
-OPERATORS = [phi0, phi1, phi2, phi3, phi4, phi5, phi6, phi7, phi8, phi9, phi10]
+OPERATORS = [phi0, phi1, phi2, phi3, phi4, phi5, phi6]
